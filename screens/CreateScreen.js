76223531ed0b87d04c10c06def482e4b5fd325be
Sufen03 <erh23976@hotmail.com>
2022-07-05 10:07:09 +0800

    create screen

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,18 +1,39 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { lightStyles, commonStyles } from "../styles/commonStyles";
 
 export default function CreateScreen({ navigation }) {
+  const styles = { ...lightStyles, ...commonStyles };
 
-  const styles = {...lightStyles, ...commonStyles}
-
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  function savePost() {}
   return (
     <View style={styles.container}>
-      <Text>
-        Create Post Screen
-      </Text>
+      <View style={{ margin: 20 }}>
+        <Text style={[additionalStyles.label, styles.text]}>Enter Title:</Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+        />
+        <Text style={[additionalStyles.label, styles.text]}>
+          Enter Content:
+        </Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={content}
+          onChangeText={(text) => setContent(text)}
+        />
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 20 }]}
+          onPress={savePost}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
 const additionalStyles = StyleSheet.create({
@@ -25,6 +46,6 @@ const additionalStyles = StyleSheet.create({
   label: {
     fontSize: 28,
     marginBottom: 10,
-    marginLeft: 5
-  }
+    marginLeft: 5,
+  },
 });
